b3097535df45df3866f16d8eccd4b995cb8dadad
atomo59 <45301512+atomo59@users.noreply.github.com>
2018-11-23 16:14:24 -1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,49 @@
 
 
+import java.awt.Color;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		EZ.initialize(20*32, 20*32);
+		EZ.setBackgroundColor(new Color(0, 0,0)); 
+		
+		Map map = new Map();
+		
+		Life lives = new Life();
+		
+		FileReader fileReader = new FileReader("level.txt");
+		Scanner fs = new Scanner(fileReader);
+
+		int width = fs.nextInt();
+		int height = fs.nextInt();
+		String inputText = fs.nextLine();
+		
+		Point[][] myPoints = new Point[width][height];
+		int i = 0;
+		for(int row = 0; row < height; row++){
+			
+			inputText = fs.nextLine();
+			
+			for (int column = 0; column < width; column++){
+				
+				char ch = inputText.charAt(column);
+		
+					if ((row == 1) && (column == 1)) {
+					}else if (ch == 'R') {
+						myPoints[column][row] = new Point(column, row);
+					}
+			}	
+		}
+		fs.close();	
+		
+		
 		int playerState = 0;
-		EZ.initialize(1024, 768);
+		
 		Player myPlayer = new Player();
 
 		while (playerState == 0) {
@@ -14,4 +53,6 @@ public class Main {
 			EZ.refreshScreen();
 		}
 	}
+		
+	
 }
