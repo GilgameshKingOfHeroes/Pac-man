03138adc1c76b92a0bb6e414b22ee68fc9929e4a
jchen2022 <45301507+jchen2022@users.noreply.github.com>
2018-11-23 15:58:15 -1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		int playerState = 0;
 		EZ.initialize(1024, 768);
-		EZ.addImage("pacman.png", 1024/2,768/2);
 		Player myPlayer = new Player();
 
 		while (playerState == 0) {
