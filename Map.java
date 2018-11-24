0e804f1ec865aa56538d97703bf48a9de425ed0e
GilgameshKingOfHeroes <tyousufleo8@gmail.com>
2018-11-23 17:49:31 -0800

    Stuff
    Main stuff for references

---
+++ Map.java
@@ -0,0 +1,51 @@
+import java.awt.Color;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class Map {
+	static EZImage grass;
+	static EZImage dirt;
+	
+	Map() throws java.io.IOException {
+		
+		FileReader fr = new FileReader("level.txt");
+		Scanner fileScanner = new Scanner(fr);
+		int width = fileScanner.nextInt();
+		int height = fileScanner.nextInt();
+		String inputText = fileScanner.nextLine();
+
+		EZ.initialize(width*32,height*32);
+		EZ.setBackgroundColor(new Color(0, 0,0)); 
+		
+		EZImage mypics[] = new EZImage[1000];
+
+
+		int i = 0;
+		for(int row = 0; row < height; row++){
+			
+			inputText = fileScanner.nextLine();
+			System.out.println(inputText);
+			
+			for (int column = 0; column < width; column++){
+				
+				char ch = inputText.charAt(column);
+		
+				switch(ch){
+					case 'R':
+						dirt = EZ.addImage("dirt.png",column*32,row*32);
+						break;		
+					case 'G':
+						grass = EZ.addImage("grass.png",column*32,row*32);
+						break;
+					default:
+						break;			
+				}
+			}
+
+			EZ.refreshScreen();
+		}
+	}
+
+}
