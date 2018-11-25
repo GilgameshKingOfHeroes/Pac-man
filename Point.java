9844fbce71beed0168c6c658387f800f5aed901c
atomo59 <45301512+atomo59@users.noreply.github.com>
2018-11-24 22:50:09 -1000

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,44 +1,45 @@
 //Alison Tomooka
-//11.23.18 V.1
+//November 2018
 
-import java.awt.Color;	//Import Color
 
 public class Point { 	//Open Point class
 
 	
-	private EZImage picture;	//declare private variable "picture" of type EZImage
+	EZImage picture;	//declare variable "picture" of type EZImage
 	
 	private int x;	//declare private variable "x" of type integer
 	private int y;	//declare private variable "y" of type integer
-	private int count;	//declare private variable "count" of type integer
-	private EZText counter;	//declare private variable "counter" of type EZText
 	
 	
 	//Class Constructor
 	Point(int column, int row) {
 		
-		x = 32;	//assign value 32 to x
-		y = 32;	//assign value 32 to y
+		x = 32*column;	//assign value 32 to x
+		y = 32*row;	//assign value 32 to y
 		
-		picture = EZ.addImage("coin.png", column*x, row*y);	//assign image to picture
-		
-		
-		count = 0; //assign value of 0 to count
-		
-		counter = EZ.addText(30, 633, "POINTS: " + count, Color.white, 10);	//assign text to counter
+		picture = EZ.addImage("coin.png", x, y);	//assign image to picture
 		
 	}
 	
 	
+
 	//Declare collected function
 	public void collected() {
-		
-			count++;	//increase count by 1
 			
 			picture.hide();	//hide picture
 			
-			counter.setMsg("POINTS: " + count);	//update counter message to new count value
-			
+	}
+	
+	
+	//Declare getX function
+	public int getX() {
+		return x;	//return value of x
+	}
+	
+	
+	//Declare getY function
+	public int getY() {
+		return y;	//return value of y
 	}
 	
 	
